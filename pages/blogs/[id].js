3c17d154d1ebc7080d619f005d0cb8389a6c6f8f
Dannon Gilbert <gilbedan@oregonstate.edu>
2020-10-09 18:32:27 -0700

    Added error checking to fetch

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -40,7 +40,7 @@ import "./blog.css";
 const ReactMarkdown = require("react-markdown");
 
 const Blog = (props) => {
-	const { className, post } = props;
+	const { className, post, error } = props;
 	const { title, coverPhoto, content, publishedDate } = post.fields;
 	const date = new Date(publishedDate);
 	const publishedDateString = dateFormatter(date.toDateString());
@@ -49,55 +49,62 @@ const Blog = (props) => {
 	return (
 		<div>
 			<Head>
-				<title>DG | {title}</title>
+				<title>DG | {title || error}</title>
 				<meta name="description" />
 			</Head>
-			<Layout contact={false}>
+			<Layout>
 				<Section>
-					<div className={className} id="capture">
-						<div className={`${className}__cover--photo`}>
-							<img
-								src={`https:${coverPhoto.fields.file.url}`}
-								alt={coverPhoto.fields.description || `${title} cover photo`}
-							/>
-						</div>
-						<div className={`${className}__data`}>
-							<p>{publishedDateString}</p>
-						</div>
-						<div className={`${className}__body`}>
-							<h1 className={`${className}__title`}>{title}</h1>
-						</div>
-						<div className={`${className}__content`}>
-							<ReactMarkdown source={content} renderers={{ code: CodeBlock }} />
-						</div>
-						<div className={`${className}__body`}>
-							<div className={`${className}__social`}>
-								<p>Share with:</p>
-								<TwitterShareButton
-									url={`https://www.dannongilbert.com/${id}`}
-									title="Check out this blog:"
-									via="dannongilbert"
-								>
-									<TwitterLogo />
-								</TwitterShareButton>
-								<LinkedinShareButton
-									url={`https://www.dannongilbert.com/${id}`}
-									title="Check out this blog:"
-									summary="Summary here"
-									source="www.dannongilbert.com"
-								>
-									<LinkedInLogo />
-								</LinkedinShareButton>
-								<EmailShareButton
-									url={`https://www.dannongilbert.com/${id}`}
-									subject="Look at this blog"
-									body="Here's a link:"
-								>
-									<EmailLogo />
-								</EmailShareButton>
+					{error ? (
+						<p>{error}</p>
+					) : (
+						<div className={className} id="capture">
+							<div className={`${className}__cover--photo`}>
+								<img
+									src={`https:${coverPhoto.fields.file.url}`}
+									alt={coverPhoto.fields.description || `${title} cover photo`}
+								/>
+							</div>
+							<div className={`${className}__data`}>
+								<p>{publishedDateString}</p>
+							</div>
+							<div className={`${className}__body`}>
+								<h1 className={`${className}__title`}>{title}</h1>
+							</div>
+							<div className={`${className}__content`}>
+								<ReactMarkdown
+									source={content}
+									renderers={{ code: CodeBlock }}
+								/>
+							</div>
+							<div className={`${className}__body`}>
+								<div className={`${className}__social`}>
+									<p>Share with:</p>
+									<TwitterShareButton
+										url={`https://www.dannongilbert.com/${id}`}
+										title="Check out this blog:"
+										via="dannongilbert"
+									>
+										<TwitterLogo />
+									</TwitterShareButton>
+									<LinkedinShareButton
+										url={`https://www.dannongilbert.com/${id}`}
+										title="Check out this blog:"
+										summary="Summary here"
+										source="www.dannongilbert.com"
+									>
+										<LinkedInLogo />
+									</LinkedinShareButton>
+									<EmailShareButton
+										url={`https://www.dannongilbert.com/${id}`}
+										subject="Look at this blog"
+										body="Here's a link:"
+									>
+										<EmailLogo />
+									</EmailShareButton>
+								</div>
 							</div>
 						</div>
-					</div>
+					)}
 				</Section>
 			</Layout>
 		</div>
@@ -109,28 +116,40 @@ Blog.defaultProps = {
 };
 
 export async function getStaticPaths() {
-	const response = await fetchBlogs();
+	try {
+		const response = await fetchBlogs();
 
-	return {
-		paths: response.map((post) => {
-			return {
-				params: {
-					id: `${post.sys.id}`,
-				},
-			};
-		}),
-		fallback: false,
-	};
+		return {
+			paths: response.map((post) => {
+				return {
+					params: {
+						id: `${post.sys.id}`,
+					},
+				};
+			}),
+			fallback: false,
+		};
+	} catch (err) {
+		console.log(err);
+	}
 }
 
 export async function getStaticProps({ params }) {
-	const post = await fetchSpecificBlog(params.id);
+	try {
+		const post = await fetchSpecificBlog(params.id);
 
-	return {
-		props: {
-			post,
-		},
-	};
+		return {
+			props: {
+				post,
+			},
+		};
+	} catch (err) {
+		return {
+			props: {
+				error: "Could not load data",
+			},
+		};
+	}
 }
 
 export default Blog;
